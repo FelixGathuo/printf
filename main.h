d2531d168fc8272f26aa557d128d90189e6ba515
FelixGathuo <gathuofelix@gmail.com>
2022-04-18 10:41:23 +0000

    printf

--- main.h
+++ main.h
@@ -1,5 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+/**
+  * struct vtype - struct vtype
+  * @tp: tp
+  * @f: function
+  *
+  */
+typedef struct vtype
+{
+	char tp;
+	void (*f)();
+} vtype_t;
 int _printf(const char *format, ...);
 
 #endif
